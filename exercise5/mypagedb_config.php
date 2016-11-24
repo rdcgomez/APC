14cf23750d7d3a9bacf499a88bb1dbb0a66fa343
davvid <davvid19996@gmail.com>
2016-11-24 09:39:24 +0800

    temporarily replaced the database name from dbtuts to mypage in mypagedb_config.php

--- exercise5/mypagedb_config.php
+++ exercise5/mypagedb_config.php
@@ -4,7 +4,7 @@
 $host = "localhost";
 $user = "root";
 $password = "";
-$datbase = "dbtuts";
+$datbase = "mypage";
 $con = mysqli_connect($host,$user,$password);
 mysqli_select_db($con,$datbase);
 ?>
