28db67f264f59b30887a13eb7d4993410b546e91
davvid <davvid19996@gmail.com>
2016-11-24 11:10:58 +0800

    error fixed

--- exercise5/mypagedatabase.php
+++ exercise5/mypagedatabase.php
@@ -6,7 +6,7 @@ include_once 'mypagedb_config.php';
 // delete condition
 if(isset($_GET['delete_id']))
 {
- $sql_query="DELETE FROM users WHERE user_id=".$_GET['delete_id'];
+ $sql_query="DELETE FROM users WHERE id=".$_GET['delete_id'];
  mysqli_query($con,$sql_query);
  header("Location: $_SERVER[PHP_SELF]");
 }
@@ -113,13 +113,13 @@ function delete_id(id)
 	{
 	?>
         <tr>
-        <td><?php echo $row[0]; ?></td>
         <td><?php echo $row[1]; ?></td>
         <td><?php echo $row[2]; ?></td>
-		<td><?php echo $row[3]; ?></td>
+        <td><?php echo $row[3]; ?></td>
 		<td><?php echo $row[4]; ?></td>
 		<td><?php echo $row[5]; ?></td>
 		<td><?php echo $row[6]; ?></td>
+		<td><?php echo $row[7]; ?></td>
 		<td align="center"><a href="javascript:edt_id('<?php echo $row[0]; ?>')"><img src="b_edit.png" align="EDIT" /></a></td>
         <td align="center"><a href="javascript:delete_id('<?php echo $row[0]; ?>')"><img src="b_drop.png" align="DELETE" /></a></td>
         </tr>
