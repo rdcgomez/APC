4359060bc9f5e78478b62424a9b1bccf0cc295a9
Renz Gomez <zemogzner@gmail.com>
2016-11-30 19:21:29 +0800

    modified

--- exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
+++ exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
@@ -1,15 +1,16 @@
-<!--ADDING DATA-->
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class mypage extends CI_Controller {
+class mypagecheckdatabase extends CI_Controller {
     public function index()
     {
 		
 		
 		$this->load->helper('url'); 
         //load view and pass the data
-        $this->load->view('my_page');
+        $this->load->view('my_page_checkdatabase');
+		
+		
 		
     }
 }
