cbcab8788d14f7bc78c10009c0936a69ca6a3b19
Renz Gomez <zemogzner@gmail.com>
2016-11-30 16:17:53 +0800

    edited the default controller in routes.php

--- exercise6/codeigniter/application/controllers/mypage.php
+++ exercise6/codeigniter/application/controllers/mypage.php
@@ -1,10 +1,11 @@
 <!--ADDING DATA-->
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+
 class mypage extends CI_Controller {
     public function index()
     {
         //load view and pass the data
-        $this->load->view('my_page', $data);
+        $this->load->view('my_page');
     }
 }
