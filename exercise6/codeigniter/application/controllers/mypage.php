560ddc5e55d12357af3d07260e199444ce5a1af2
Renz Gomez <zemogzner@gmail.com>
2016-11-30 15:31:48 +0800

    trying

--- exercise6/codeigniter/application/controllers/mypage.php
+++ exercise6/codeigniter/application/controllers/mypage.php
@@ -1,14 +1,14 @@
 <!--ADDING DATA-->
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-class Home extends CI_Controller {
+class mypage extends CI_Controller {
     public function index()
     {
         //load model
         $this->load->model('mypage_model');
         
         //get data from the database
-        $data['images'] = $this->mypage_model->get_images();
+        $data['dbtuts'] = $this->mypage_model->get_images();
         
         //load view and pass the data
         $this->load->view('my_page', $data);
