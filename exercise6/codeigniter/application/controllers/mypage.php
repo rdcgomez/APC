4652c875afde033a090cc9b4c931e6279553b57c
Renz Gomez <zemogzner@gmail.com>
2016-11-30 17:04:09 +0800

    saved

--- exercise6/codeigniter/application/controllers/mypage.php
+++ exercise6/codeigniter/application/controllers/mypage.php
@@ -5,7 +5,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mypage extends CI_Controller {
     public function index()
     {
+		
+		
+		$this->load->helper('url'); 
         //load view and pass the data
         $this->load->view('my_page');
+		
     }
 }
