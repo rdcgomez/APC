cb65e3bcfb7429e4616e96139ffdcee2a1c5fbae
Renz Gomez <zemogzner@gmail.com>
2016-11-30 16:15:24 +0800

    modified

--- exercise6/codeigniter/application/controllers/mypage.php
+++ exercise6/codeigniter/application/controllers/mypage.php
@@ -4,12 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mypage extends CI_Controller {
     public function index()
     {
-        //load model
-        $this->load->model('mypage_model');
-        
-        //get data from the database
-        $data['dbtuts'] = $this->mypage_model->get_images();
-        
         //load view and pass the data
         $this->load->view('my_page', $data);
     }
