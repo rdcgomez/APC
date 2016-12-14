b36cc196e7ab054aafc22acf25d18984f79e17c5
Renz Gomez <zemogzner@gmail.com>
2016-12-15 00:50:58 +0800

    added data_submitted function in mypage.php

--- exercise6/codeigniter/application/controllers/mypage.php
+++ exercise6/codeigniter/application/controllers/mypage.php
@@ -7,14 +7,20 @@ class mypage extends CI_Controller {
     parent::__construct();
     $this->load->helper('url');
     }
-  
 
     public function index(){
-
     $this->load->model('mypage_add');
     //load view and pass the data
     $this->load->view('my_page');
+    }
 
+    public function data_submitted() {
+    $data = array(
+    '$first_name' => $this->input->post('first_name'),
+    '$last_name' => $this->input->post('last_name')
+    );
+    // Show submitted data in visitors database
+    $this->load->view("my_page_checkdatabase", $data);
+    }
 
-  }
 }
