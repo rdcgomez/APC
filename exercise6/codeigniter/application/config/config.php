93a623cf744100ae79d802b5a074d443d2c685fc
Renz Gomez <zemogzner@gmail.com>
2016-12-14 20:58:16 +0800

    remove index.php in config.php and added codes in .htaccess

--- exercise6/codeigniter/application/config/config.php
+++ exercise6/codeigniter/application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://localhost/exercise6/codeigniter/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
