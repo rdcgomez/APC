2983ca86faf5c3482b7821449547465bf968498f
Renz Gomez <zemogzner@gmail.com>
2016-12-14 22:15:09 +0800

    adding the model in the controllers

--- exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
+++ exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
@@ -4,9 +4,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mypagecheckdatabase extends CI_Controller {
     public function index()
     {
-		//$this->load->model('mypage_model');
-        //load view and pass the data
-        $this->load->view('my_page_checkdatabase');
+
+		$this->load->model('mypage_add');
+    
+    //load view and pass the data
+    $this->load->view('my_page_checkdatabase');
 
     }
 }
