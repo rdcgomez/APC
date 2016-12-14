5835d7f58fc17c9bc5bf6402b47e6037ab9ece8b
Renz Gomez <zemogzner@gmail.com>
2016-12-14 16:54:46 +0800

    created $data['sql_query'] = $this->mypage_model->dbtuts(); in mypage.php

--- exercise6/codeigniter/application/controllers/mypage.php
+++ exercise6/codeigniter/application/controllers/mypage.php
@@ -4,11 +4,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mypage extends CI_Controller {
     public function index()
     {
-		
-		
-		$this->load->helper('url'); 
+
+
+		$this->load->model('mypage_model');
+
+
+     $data['sql_query'] = $this->mypage_model->dbtuts();
         //load view and pass the data
         $this->load->view('my_page');
-		
+
     }
 }
