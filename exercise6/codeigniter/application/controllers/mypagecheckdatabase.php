960a5d17bf9e283ad1961f680f5d5fda6ca751af
Renz Gomez <zemogzner@gmail.com>
2016-12-14 23:03:01 +0800

    remove 2 lines in mypagecheckdatabase.php

--- exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
+++ exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
@@ -4,9 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mypagecheckdatabase extends CI_Controller {
     public function index()
     {
-
-		$this->load->model('mypage_add');
-    $data['sql_query'] = $this->mypageadd->dbtuts();
+      
     //load view and pass the data
     $this->load->view('my_page_checkdatabase', $data);
 
