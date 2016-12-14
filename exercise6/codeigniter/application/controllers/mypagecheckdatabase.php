e021d35d81d2e453e75731e536c5d93cd63eb9d2
Renz Gomez <zemogzner@gmail.com>
2016-12-14 23:03:44 +0800

    removed $data in line 9 of mypagecheckdatabase.php

--- exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
+++ exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
@@ -4,9 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class mypagecheckdatabase extends CI_Controller {
     public function index()
     {
-      
+
     //load view and pass the data
-    $this->load->view('my_page_checkdatabase', $data);
+    $this->load->view('my_page_checkdatabase');
 
     }
 }
