c3d04aa4b96d07149b154507d857bd1d4e80e77f
Renz Gomez <zemogzner@gmail.com>
2016-12-14 16:57:12 +0800

    added comments

--- exercise6/codeigniter/application/controllers/mypage.php
+++ exercise6/codeigniter/application/controllers/mypage.php
@@ -5,13 +5,14 @@ class mypage extends CI_Controller {
     public function index()
     {
 
-
+    //load model
 		$this->load->model('mypage_model');
 
+    //get data from the database
+    $data['sql_query'] = $this->mypage_model->dbtuts();
 
-     $data['sql_query'] = $this->mypage_model->dbtuts();
-        //load view and pass the data
-        $this->load->view('my_page');
+    //load view and pass the data
+    $this->load->view('my_page');
 
     }
 }
