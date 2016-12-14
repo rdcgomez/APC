bc927da585a3df22c4541be66665957cbf391ca1
Renz Gomez <zemogzner@gmail.com>
2016-12-14 19:13:23 +0800

    change line 87 in my_page_checkdatabase.php....<a> href link oh Homepage changed | commented out line 7 in mypagecheckdatabase.php temporarily.

---
+++ exercise6/codeigniter/application/controllers/mypagecheckdatabase.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class mypagecheckdatabase extends CI_Controller {
+    public function index()
+    {
+		$this->load->model('mypage_model');
+        //load view and pass the data
+        $this->load->view('my_page_checkdatabase');
+
+    }
+}
